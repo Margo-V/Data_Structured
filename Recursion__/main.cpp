75b4295e104a65e92f9dc18cc746589b16e34784
Margo-V <flautorfei1929@gmail.com>
2021-10-03 13:49:13 +0400

    Recursion DONE

--- Recursion__/main.cpp
+++ Recursion__/main.cpp
@@ -9,11 +9,16 @@ void elevator(int level)
 	elevator(level - 1);	//Рекурсивный вызов 
 	cout << level << endl;
 }
+//#define FACTORIAL
+//#define FACTORIAL_2
+//#define POWER
+//#define POWER_2
 
-int factorial(int number, int result =1)
+#ifdef FACTORIAL
+int factorial(int number, int result = 1)
 {
-	
-	if (number == 1 || number == 0)
+
+	if (/*number == 1 || */number == 0)
 	{
 		return result;
 	}
@@ -22,15 +27,79 @@ int factorial(int number, int result =1)
 		factorial(number - 1, result *= number);
 	}
 }
+#endif // FACTORIAL
+
+#ifdef POWER
+int power(int number, int pow)
+{
+	if (pow == 0)return 1;
+
+	if (pow == 1)
+	{
+		return number;
+	}
+	else
+	{
+		result
+			return  power(number, pow - 1);
+	}
+
+	//return pow == 1 ? number : power(number * number, pow - 1);
+}
+#endif // POWER
+
+#ifdef FACTORIAL_2
+int factorial(int n)
+{
+	/*if (n == 0)return 1;
+	return n * factorial(n - 1);*/
+
+	return n == 0 ? 1 : n * factorial(n - 1);
+}
+#endif // FACTORIAL_2
+
+#ifdef POWER_2
+double power(double a, int n)
+{
+	//if (n == 0)return 1;
+	/*if (n > 0)return  a * power(a, n - 1);
+	else return 1 / a * power(a, n + 1);*/
+	return n == 0 ? 1 : n > 0 ? a * power(a, n - 1) : 1 / a * power(a, n + 1);
+}
+#endif // POWER_2
+
 
 void main()
 {
 	setlocale(LC_ALL, "rus");
 	//cout << "Hello world!" << endl;
 	//main();	//Рекурсивный вызов функции main()
-	int n;
 	//cout << "Введите номер этажа: "; cin >> n;
 	//elevator(n);
+#ifdef FACTORIAL
+	//int n, m;
 	cout << "Введите число: "; cin >> n;
 	cout << factorial(n);
+	//cout << "Введите число: "; cin >> n;
+	//cout << "Введите степень числа: "; cin >> m;
+	//cout << power(n, m);
+#endif // FACTORIAL
+
+#ifdef FACTORIAL_2
+	cout << "Введите число для ввычисления факториала: "; cin >> n;
+	cout << factorial(n) << endl;
+#endif // FACTORIAL_2
+
+#ifdef POWER_2
+	int a; //Основание степени
+	int n; //Показатель степени
+	cout << "Введите основание степени: "; cin >> a;
+	cout << "Введите показатель степени: "; cin >> n;
+	cout << power(a, n) << endl;
+
+	main();
+#endif // POWER_2
+
+
+
 }
