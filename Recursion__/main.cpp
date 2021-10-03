0c91fa8c14ec00794b70059c358164c0f41c7667
Margo-V <flautorfei1929@gmail.com>
2021-10-03 11:33:56 +0400

    factorial

--- Recursion__/main.cpp
+++ Recursion__/main.cpp
@@ -9,12 +9,28 @@ void elevator(int level)
 	elevator(level - 1);	//–екурсивный вызов 
 	cout << level << endl;
 }
+
+int factorial(int number, int result =1)
+{
+	
+	if (number == 1 || number == 0)
+	{
+		return result;
+	}
+	else
+	{
+		factorial(number - 1, result *= number);
+	}
+}
+
 void main()
 {
 	setlocale(LC_ALL, "rus");
 	//cout << "Hello world!" << endl;
 	//main();	//–екурсивный вызов функции main()
 	int n;
-	cout << "¬ведите номер этажа: "; cin >> n;
-	elevator(n);
+	//cout << "¬ведите номер этажа: "; cin >> n;
+	//elevator(n);
+	cout << "¬ведите число: "; cin >> n;
+	cout << factorial(n);
 }
