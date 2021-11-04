de45b277a4d268dd5bad8258cad177206ea6d224
Margo-V <flautorfei1929@gmail.com>
2021-11-04 23:52:29 +0300

    Trying to repeat binery tree by self

--- BineryTree/main.cpp
+++ BineryTree/main.cpp
@@ -1,193 +1,98 @@
 #include <iostream>
 using namespace std;
 
+#define tab "\t"
+
 class Tree
 {
-	class Element
+	class Node
 	{
 		int Data;
-		Element* pLeft;
-		Element* pRight;
+		Node* pLeft;
+		Node* pRight;
 	public:
-		Element(int Data, Element* pLeft = nullptr, Element* pRight = nullptr) 
+		Node(int Data, Node* pLeft = nullptr, Node* pRight = nullptr)
 			:Data(Data), pLeft(pLeft), pRight(pRight)
 		{
-#ifdef DEBUG
-			this->Data = Data;
-			this->pLeft = pLeft;
-			this->pRight;
-#endif // DEBUG
-			cout << "EConstructor: " << this << endl;
+			cout << "NConstructor:" << tab << this << endl;
 		}
-		~Element()
+		~Node()
 		{
-			cout << "EDestructor: " << this << endl;
+			cout << "NDestructor:" << tab << this << endl;
 		}
 		friend class Tree;
 	}*Root;
+
 public:
-	Element* getRoot()const
-	{
-		return Root;
-	}
 	Tree()
 	{
 		this->Root = nullptr;
-		cout << "TConstructor: " << this << endl;
-	}
-	Tree(const std::initializer_list<int>& il)
-	{
-		for (int const* it = il.begin(); it != il.end(); it++)
-		{
-			insert(*it, this->Root);
-		}
+		cout << "TConstructor" << tab << this << endl;
 	}
 	~Tree()
 	{
-		clear(Root);
-		cout << "TDestructor: " << this << endl;
+		cout << "TDestructor:" << tab << this << endl;
 	}
-	void insert(int Data)
+	Node* getRoot()const
 	{
-		insert(Data, Root);
+		return Root;
 	}
-	void erase(int Data)
-	{
-		return erase(Data, this->Root);
-	}
-	void print()const
-	{
-		print(this->Root);
-		cout << endl;
-	}
-	int minValue()const
-	{
-		return minValue(Root);
-	}
-	int maxValue()const
-	{
-		return maxValue(Root);
-	}
-	int count()const
-	{
-		return count(Root);
-	}
-	int sum()const
-	{
-		return sum(Root);
-	}
-	double avg()const
-	{
-		return (double)sum(Root) / count(Root);
-	}
-private:
-	void insert(int Data, Element* Root)
+	void insert(int Data, Node* Root)
 	{
 		if (this->Root == nullptr)
 		{
-			this->Root = new Element(Data);
+			this->Root = new Node(Data);
 			return;
 		}
 		if (Root == nullptr)return;
 		if (Data < Root->Data)
 		{
-			if (Root->pLeft == nullptr)Root->pLeft = new Element(Data);
+			if (Root->pLeft == nullptr)Root->pLeft = new Node(Data);
 			else insert(Data, Root->pLeft);
 		}
-		else if(Data >Root->Data)
+		else if (Data > Root->Data)
 		{
-			if (Root->pRight)insert(Data, Root->pRight);
-			else Root->pRight = new Element(Data);
+			if (Root->pRight) insert(Data, Root->pRight);
+			else Root->pRight = new Node(Data);
 		}
 	}
-	void erase(int Data, Element*& Root)
-	{
-		if (Root == nullptr)return;
-		erase(Data, Root->pLeft);
-		erase(Data, Root->pRight);
-		if (Data == Root->Data)
-		{
-			if (Root->pLeft == nullptr && Root->pRight == nullptr)
-			{
-				delete Root;
-				Root = nullptr;
-			}
-			else
-			{
-				if (count(Root->pLeft) > count(Root->pRight))
-				{
-					Root->Data = maxValue(Root->pLeft);
-					erase(maxValue(Root->pLeft), Root->pLeft);
-				}
-				else 
-				{
-					Root->Data = minValue(Root->pRight);
-					erase(minValue(Root->pRight), Root->pRight);
-				}
-			}		
-		}
-	}
-	int minValue(Element* Root)const
+	int minValue(Node* Root)const
 	{
 		/*if (Root->pLeft == nullptr)return Root->Data;
 		else return minValue(Root->pLeft);*/
-		return Root->pLeft ? minValue(Root->pLeft) : Root->Data;
+		Root->pLeft ? minValue(Root->pLeft) : Root->Data;
 	}
-	int maxValue(Element* Root)const
+	int maxValue(Node* Root)const
 	{
-		return Root->pRight ? maxValue(Root->pRight) : Root->Data;
+		Root->pRight ? maxValue(Root->pRight) : Root->Data;
 	}
-	int count(Element* Root)const
+	int count(Node* Root)const
 	{
-		/*if (Root == nullptr)return 0;
-		else return count(Root->pLeft) + count(Root->pRight) + 1;*/
-		return Root == nullptr ? 0 : count(Root->pLeft) + count(Root->pRight) + 1;
-		//return Root ? count(Root->pLeft) + count(Root->pRight) + 1 : 0;
+		if (Root == nullptr)return 0;
+		else return count(Root->pLeft) + count(Root->pRight) + 1;
 	}
-	int sum(Element* Root)const
+	int sum(Node* Root)const
 	{
-		//return Root == nullptr ? 0 : sum(Root->pLeft) + sum(Root->pRight) + Root->Data;
-		return !Root ? 0 : sum(Root->pLeft) + sum(Root->pRight) + Root->Data;
+		return !Root ? 0 :  sum(Root->pLeft) + sum(Root->pRight) + Root->Data;
 	}
-	void clear(Element* Root)
-	{
-		if (Root == nullptr)return;
-		clear(Root->pLeft);
-		clear(Root->pRight);
-		delete Root;
-	}
-	void print(Element* Root)const
+	void print(Node* Root)const
 	{
 		if (Root == nullptr)return;
 		print(Root->pLeft);
-		cout << Root->Data << "\t";
+		cout << Root->Data << tab;
 		print(Root->pRight);
 	}
 };
 
-//#define BASE_CHECK
-
 void main()
 {
 	setlocale(LC_ALL, "ru");
-#ifdef BASE_CHECK
-	int n;
-	cout << "Введите размер дерева: "; cin >> n;
-	Tree tree;
-	for (int i = 0; i < n; i++)
-		tree.insert(rand() % 100);
-	tree.print();
-	cout << "Минимальное значение в дереве: " << tree.minValue() << endl;
-	cout << "Минимальное значение в дереве: " << tree.maxValue() << endl;
-	cout << "Количество элементов в дереве: " << tree.count() << endl;
-	cout << "Сумма элементов в дереве: " << tree.sum() << endl;
-	cout << "Среднее арифметическое элементов в дереве: " << tree.avg() << endl;
-#endif // BASE_CHECK
 
-	Tree tree = { 50, 25, 80, 16, 32, 64, 85, 58, 75, 84, 91 };
-	tree.print();
-	int value;
-	cout << "Введите удаляемое значение: "; cin >> value;
-	tree.erase(value);
-	tree.print();
+	Tree tree;
+	tree.insert(57, nullptr);
+	tree.insert(56, tree.getRoot());
+	tree.insert(86, tree.getRoot());
+	tree.insert(73, tree.getRoot());
+	tree.print(tree.getRoot());
+	cout << endl << tree.count(tree.getRoot()) << endl;
 }
